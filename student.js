37b5801ba609415d9ea86ed327f7aa45881c62ad
merilynlariosa71-a11y <merilynlariosa71@gmail.com>
2026-04-24 10:39:54 +0800

    Add files via upload

---
+++ student.js
@@ -0,0 +1,11 @@
+// Student Information
+let name = "Your Name";
+let course = "BSIT";
+let year = "2nd Year";
+let favoriteLanguage = "JavaScript";
+
+// Display Output
+console.log("Name: " + name);
+console.log("Course: " + course);
+console.log("Year Level: " + year);
+console.log("Favorite Programming Language: " + favoriteLanguage);
